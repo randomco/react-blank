abcbd8bf6df00a7a591f01143d75b19ffaeef722
Prabhakar Desai <111271@cognizant.com>
2020-10-13 22:56:41 +0530

    init commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Introduction
+This is a simple React JS application.
+
+# How to Build
+**Run**
+
+	npm start
+
+___________________
+**Build**
+
+	npm run-script build
---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import RecipeReviewCard from './RecipeReviewCard';
+
+function App({config, data}) {
+    return (
+        <div className="App">
+            <header className="App-header" style={{backgroundColor: config.color}}>
+                <img src="https://svgshare.com/i/QWP.svg" className="App-logo" alt="logo" />
+                <p>
+                    Hello, I am a &nbsp;
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        React
+                    </a>
+                    &nbsp; Widget running on <br/>
+                    <code>~ AWS Cloud! ~</code>
+                </p>
+                <RecipeReviewCard data={data} config={config}/>
+            </header>
+        </div>
+    );
+}
+
+export default App;
